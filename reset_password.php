48b41ec1c928cc1afcd7d7bac2cbe7afc94fbc94
Irenecsw <167046584+Irenecsw@users.noreply.github.com>
2025-10-15 04:41:35 +0800

    Changed database name

--- reset_password.php
+++ reset_password.php
@@ -18,7 +18,7 @@ function sendJsonResponse($success, $message) {
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "saveplate";
+$dbname = "test"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
